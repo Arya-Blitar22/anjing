8b4add0e52b91a1364ac09024cd011ccce7565f6
Arya <92986716+Arya-Blitar22@users.noreply.github.com>
2023-07-19 11:04:57 +0700

    Update dropbear-ws.py

--- sshws/dropbear-ws.py
+++ sshws/dropbear-ws.py
@@ -12,7 +12,7 @@ PASS = ''
 BUFLEN = 4096 * 4
 TIMEOUT = 60
 DEFAULT_HOST = '127.0.0.1:109'
-RESPONSE = 'HTTP/1.1 101 <font color="green"><b><i>VPN BY MADEST</font></b></i>\r\nContent-Length: 104857600000\r\n\r\n'
+RESPONSE = 'HTTP/1.1 101 <font color="green"><b><i>Direct Tuneling</font></b></i>\r\nContent-Length: 104857600000\r\n\r\n'
 
 class Server(threading.Thread):
     def __init__(self, host, port):
